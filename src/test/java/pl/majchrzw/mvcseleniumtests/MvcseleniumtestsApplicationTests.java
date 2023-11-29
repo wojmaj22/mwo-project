107738433897651969fc0c9e67c99ddaec9a7f49
Wojciech <wojjmaj22@gmail.com>
2023-11-29 12:22:09 +0100

    test corrected

--- src/test/java/pl/majchrzw/mvcseleniumtests/MvcseleniumtestsApplicationTests.java
+++ src/test/java/pl/majchrzw/mvcseleniumtests/MvcseleniumtestsApplicationTests.java
@@ -1,13 +1,6 @@
 package pl.majchrzw.mvcseleniumtests;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MvcseleniumtestsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
